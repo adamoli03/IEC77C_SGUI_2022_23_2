4d4d57b359f0e46fb23a6f2f85be6baab5b0e790
fagyo <fagyo@LAPTOP-IKANSRU9>
2022-10-13 16:32:36 +0200

    Add project files.

---
+++ IEC77C_ADT_2022_23_1.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IEC77C_ADT_2022_23_1.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ IEC77C_ADT_2022_23_1.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IEC77C_ADT_2022_23_1.Repository
+{
+    public class Class1
+    {
+    }
+}
