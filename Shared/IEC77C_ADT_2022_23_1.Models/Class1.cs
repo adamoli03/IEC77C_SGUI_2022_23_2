3fcb408f7b78acbd8a4d479733178aac09cda856
Piroska Ádám <adamolii03@gmail.com>
2022-10-19 23:49:33 +0200

    Added DataBase file (Could only add to a windows form application) Created Store, Company, City classes

--- Shared/IEC77C_ADT_2022_23_1.Models/Class1.cs
+++ Shared/IEC77C_ADT_2022_23_1.Models/Class1.cs
@@ -2,7 +2,30 @@
 
 namespace IEC77C_ADT_2022_23_1.Models
 {
-    public class Class1
+    public class Store
     {
+        int Store_Id { get; set; }
+        string Company_ID { get; set; }
+        string City_ID
+        string Adress { get; set; }
+        int Size { get; set; } //squareMeters
     }
+
+    public class Company
+    {
+        int Company_ID { get; set; }
+        string Name { get; set; }
+        int networth { get; set; }
+    }
+
+    public class City
+    {
+        int City_ID { get; set; }
+        string City_Name { get; set; }
+        string State { get; set; }
+        string Country { get; set; }
+    
+    }
+
+
 }
