087e722c59b036a9aeede6e78a5af49b6c3f872f
Piroska Ádám <adamolii03@gmail.com>
2022-10-20 22:33:03 +0200

    Database changes, trying to fix NU1104 error

--- Backend/IEC77C_ADT_2022_23_1.Data/Class1.cs
+++ Backend/IEC77C_ADT_2022_23_1.Data/Class1.cs
@@ -2,7 +2,30 @@
 
 namespace IEC77C_ADT_2022_23_1.Data
 {
-    public class Class1
+    //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True; MultipleActiveResultSets=True;
+    /*public class Store
     {
+        int Store_Id { get; set; }
+        string Company_ID { get; set; }
+        string City_ID
+        string Adress { get; set; }
+        int Size { get; set; } //squareMeters
     }
+
+    public class Company
+    {
+        int Company_ID { get; set; }
+        string Name { get; set; }
+        int networth { get; set; }
+    }
+
+    public class City
+    {
+        int City_ID { get; set; }
+        string City_Name { get; set; }
+        string State { get; set; }
+        string Country { get; set; }
+
+    }*/
+
 }
