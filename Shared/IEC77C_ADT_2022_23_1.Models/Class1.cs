d3c76cdcb3e223c7d1e9b4c330e2efba20813f00
Piroska Ádám <adamolii03@gmail.com>
2022-10-20 23:47:07 +0200

    Created constructors

--- Shared/IEC77C_ADT_2022_23_1.Models/Class1.cs
+++ Shared/IEC77C_ADT_2022_23_1.Models/Class1.cs
@@ -6,6 +6,15 @@ namespace IEC77C_ADT_2022_23_1.Models
 {
     public class Store
     {
+        public Store(int store_Id, string company_ID, string city_ID, string adress, int size)
+        {
+            Store_Id = store_Id;
+            Company_ID = company_ID;
+            City_ID = city_ID;
+            Adress = adress;
+            Size = size;
+        }
+
         int Store_Id { get; set; }
         string Company_ID { get; set; }
         string City_ID { get; set; }
@@ -15,6 +24,13 @@ namespace IEC77C_ADT_2022_23_1.Models
 
     public class Company
     {
+        public Company(int company_ID, string name, int networth)
+        {
+            Company_ID = company_ID;
+            Name = name;
+            this.networth = networth;
+        }
+
         int Company_ID { get; set; }
         string Name { get; set; }
         int networth { get; set; }
@@ -22,6 +38,14 @@ namespace IEC77C_ADT_2022_23_1.Models
 
     public class City
     {
+        public City(int city_ID, string city_Name, string state, string country)
+        {
+            City_ID = city_ID;
+            City_Name = city_Name;
+            State = state;
+            Country = country;
+        }
+
         int City_ID { get; set; }
         string City_Name { get; set; }
         string State { get; set; }
