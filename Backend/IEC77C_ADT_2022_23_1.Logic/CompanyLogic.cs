2a9cc45cc7c5205f5a03e99df6e7bb30bc9ac2fb
Piroska Ádám <adamolii03@gmail.com>
2022-11-19 18:05:48 +0100

    Created CompanyLogic TODO: Create NON-CRUD Methods

--- Backend/IEC77C_ADT_2022_23_1.Logic/CompanyLogic.cs
+++ Backend/IEC77C_ADT_2022_23_1.Logic/CompanyLogic.cs
@@ -4,11 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using IEC77C_ADT_2022_23_1.Repository;
+using IEC77C_ADT_2022_23_1.Models;
+
 
 namespace IEC77C_ADT_2022_23_1.Logic
 {
     class CompanyLogic
     {
-        
+        ICompanyRepository companyrepo;
+
+        public CompanyLogic(ICompanyRepository companyrepo)
+        {
+            this.companyrepo = companyrepo;
+        }
+
+        public IList<Company> GetAll()
+        {
+            return companyrepo.GetAll().ToList();
+        }
+
+        public void Add(Company entity)
+        {
+            companyrepo.Add(entity);
+        }
+
+        public void Update(Company entity)
+        {
+            companyrepo.Update(entity);
+        }
+
+        public void Delete(Company entity)
+        {
+            companyrepo.Delete(entity);
+        }
+
+        public Company FindByID(int ID)
+        {
+            return companyrepo.FindById(ID);
+        }
+
+        //TODO: CREATE NON CRUD METHODS
     }
 }
