0cbb705f916f12b084b09c8b2e74f43a09c3c086
Piroska Ádám <adamolii03@gmail.com>
2022-11-19 18:00:05 +0100

    Created City Logic - TODO NON CRUD

---
+++ Backend/IEC77C_ADT_2022_23_1.Logic/CityLogic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using IEC77C_ADT_2022_23_1.Repository;
+using IEC77C_ADT_2022_23_1.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace IEC77C_ADT_2022_23_1.Logic
+{
+    public class CityLogic
+    {
+        ICityRepository cityrepo;
+        public CityLogic(ICityRepository cityrepo)
+        {
+            this.cityrepo = cityrepo;
+        }
+        public IList<City> GetAll()
+        {
+            return cityrepo.GetAll().ToList();
+        }
+
+        public void Update(City city)
+        {
+            cityrepo.Update(city);
+        }
+
+        public void Add(City city)
+        {
+            cityrepo.Add(city);
+        }
+
+        public void Delete(City entity)
+        {
+            cityrepo.Delete(entity);
+        }
+
+        public City FindById(int ID)
+        {
+            return cityrepo.FindById(ID);
+        }
+
+        //NON-CRUD METHODS -TODO
+    }
+}
---
+++ Backend/IEC77C_ADT_2022_23_1.Logic/CompanyLogic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IEC77C_ADT_2022_23_1.Repository;
+
+namespace IEC77C_ADT_2022_23_1.Logic
+{
+    class CompanyLogic
+    {
+        
+    }
+}
